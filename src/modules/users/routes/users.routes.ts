9c93eaec0a4e2c992b75a394be4676e4e27b55e8
Bruno Puzoni <bpuzoni@gmail.com>
2021-06-01 21:59:23 -0300

    validacao dos requests de usuario

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,45 @@
 import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
+
 import UsersController from "../controllers/UsersController";
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.get("/", usersController.index);
-usersRouter.get("/:id", usersController.show);
-usersRouter.post("/", usersController.create);
+
+usersRouter.get(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  usersController.show,
+);
+
+usersRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      user: {
+        nome: Joi.string().required(),
+        sobrenome: Joi.string().required(),
+        email: Joi.string().email().required(),
+        senha: Joi.string().required(),
+        sexo: Joi.string().required(),
+        data_nasc: Joi.date().required(),
+        telefone: Joi.string().required(),
+        end_cep: Joi.string().required(),
+        end_rua: Joi.string().required(),
+        end_num: Joi.number().required(),
+        end_cidade: Joi.string().required(),
+        end_bairro: Joi.string().required(),
+        end_uf: Joi.string().required(),
+      },
+    },
+  }),
+  usersController.create,
+);
 
 export default usersRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import cors from "cors";
+import { errors } from "celebrate";
+
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
 import "@shared/typeorm";
@@ -12,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
